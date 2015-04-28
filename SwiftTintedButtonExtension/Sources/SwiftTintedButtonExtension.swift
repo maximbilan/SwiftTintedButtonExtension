d5c8d6bdb09cd6f776936e1710bba3fbf581aaf9
Maxim Bilan <maximb.mail@gmail.com>
2015-04-28 15:49:47 +0300

    Added extention

---
+++ SwiftTintedButtonExtension/Sources/SwiftTintedButtonExtension.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftTintedButtonExtension.swift
+//  SwiftTintedButtonExtension
+//
+//  Created by Maxim Bilan on 4/28/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import Foundation
