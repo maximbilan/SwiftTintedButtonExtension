6b687c6f3513372869248b2b97b7316bf4af65ab
Maxim Bilan <maximb.mail@gmail.com>
2015-04-28 16:16:26 +0300

    Modified extention

--- SwiftTintedButtonExtension/Sources/SwiftTintedButtonExtension.swift
+++ SwiftTintedButtonExtension/Sources/SwiftTintedButtonExtension.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIButton {
+	
+	public func setImageTintColor(color: UIColor, state: UIControlState) {
+		let image = self.imageForState(state)
+		if image != nil {
+			self.setImage(self.tintedImageWithColor(color, image: image!), forState: state)
+		}
+	}
+	
+	public func setBackgroundTintColor(color: UIColor, state: UIControlState) {
+		let backgroundImage = self.backgroundImageForState(state)
+		if backgroundImage != nil {
+			self.setBackgroundImage(self.tintedImageWithColor(color, image: backgroundImage!), forState: state)
+		}
+	}
+	
+	private func tintedImageWithColor(tintColor: UIColor, image: UIImage) -> UIImage {
+		
+		UIGraphicsBeginImageContextWithOptions(image.size, false, UIScreen.mainScreen().scale)
+		
+		let context = UIGraphicsGetCurrentContext()
+		CGContextTranslateCTM(context, 0, image.size.height)
+		CGContextScaleCTM(context, 1.0, -1.0)
+		
+		let rect = CGRectMake(0, 0, image.size.width, image.size.height)
+		
+		CGContextSetBlendMode(context, kCGBlendModeNormal)
+		CGContextDrawImage(context, rect, image.CGImage)
+		
+		CGContextSetBlendMode(context, kCGBlendModeSourceIn)
+		tintColor.setFill()
+		CGContextFillRect(context, rect)
+		
+		let coloredImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		
+		return coloredImage
+	}
+}
